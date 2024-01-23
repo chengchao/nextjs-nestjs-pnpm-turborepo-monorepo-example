e4c3e299f50f5e1c24b81c9379bdc0753eae478c
chengchao <chengchao60827@gmail.com>
2024-01-22 23:06:06 -0800

    feat: add commitlint config of pnpm workspace

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -1,5 +1,8 @@
 module.exports = {
-  extends: ["@commitlint/config-conventional"],
+  extends: [
+    "@commitlint/config-conventional",
+    "@commitlint/config-pnpm-scopes",
+  ],
   rules: {
     "body-leading-blank": [1, "always"],
     "body-max-line-length": [2, "always", 100],
